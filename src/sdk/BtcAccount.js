c0de0cddb599348f53f48a1edd9bfe4a1e4696f5
sickworm <ch.operation@gmail.com>
2018-06-21 14:45:24 +0800

    fix direction bug, fuck

--- src/sdk/BtcAccount.js
+++ src/sdk/BtcAccount.js
@@ -114,7 +114,7 @@ export default class BtcAccount {
     txInfo.value = 0
     txInfo.value -= txInfo.inputs.reduce((sum, input) => sum + input.isMine ? input.value : 0, 0)
     txInfo.value += txInfo.outputs.reduce((sum, output) => sum + output.isMine ? output.value : 0, 0)
-    let input = txInfo.inputs.find(input => addressInfo.address === input.prevAddress)
+    let input = txInfo.inputs.find(input => input.isMine)
     txInfo.direction = input ? D.tx.direction.out : D.tx.direction.in
 
     // check utxo update. unspent can update to pending and spent, pending can update to spent. otherwise ignore
--- src/sdk/EthAccount.js
+++ src/sdk/EthAccount.js
@@ -109,7 +109,7 @@ export default class EthAccount {
     txInfo.value = 0
     txInfo.value -= txInfo.inputs.reduce((sum, input) => sum + input.isMine ? input.value : 0, 0)
     txInfo.value += txInfo.outputs.reduce((sum, output) => sum + output.isMine ? output.value : 0, 0)
-    let input = txInfo.inputs.find(input => addressInfo.address === input.prevAddress)
+    let input = txInfo.inputs.find(input => input.isMine)
     txInfo.direction = input ? D.tx.direction.out : D.tx.direction.in
 
     // update account info
