32ee69836b9d58c552a85c910106d8180d003a1d
sickworm <ch.operation@gmail.com>
2018-08-02 11:27:32 +0800

    fix eswallet.js can not export

--- build/webpack.config.chrome.sdk.js
+++ build/webpack.config.chrome.sdk.js
@@ -3,7 +3,14 @@ const webpackBase = require('./webpack.config.dev.js');
 
 let cfg = Object.assign(webpackBase, {
   devtool: 'cheap-module-source-map',
-  entry: ['babel-polyfill', path.resolve(__dirname, '../index.js')],
+  entry: [path.resolve(__dirname, '../index.js')],
+
+  output: {
+    filename: 'eswallet.js',
+    path: path.resolve(__dirname, '../dist'),
+    library: 'eswallet',
+    libraryTarget: 'commonjs2'
+  },
 });
 
 module.exports = cfg;
