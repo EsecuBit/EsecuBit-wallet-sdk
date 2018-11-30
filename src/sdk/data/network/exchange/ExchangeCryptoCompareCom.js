db9efb143e76297ccb616ba258c0b69d79f584d2
sickworm <ch.operation@gmail.com>
2018-11-30 15:40:32 +0800

    optimize ExchangeCryptoCompare coinType

--- src/sdk/data/network/exchange/ExchangeCryptoCompareCom.js
+++ src/sdk/data/network/exchange/ExchangeCryptoCompareCom.js
@@ -1,27 +1,7 @@
 
 import D from '../../../D'
 
-let UPDATE_DURATION = 10 * 60 * 1000
-
-const UNITS = {}
-UNITS[D.coin.main.btc] = D.unit.btc.BTC
-UNITS[D.coin.test.btcTestNet3] = D.unit.btc.BTC
-UNITS[D.coin.main.eth] = D.unit.eth.Ether
-UNITS[D.coin.test.ethRinkeby] = D.unit.eth.Ether
-UNITS[D.coin.main.eos] = D.unit.eos.EOS
-UNITS[D.coin.test.eosJungle] = D.unit.eos.EOS
-UNITS[D.coin.test.eosKylin] = D.unit.eos.EOS
-UNITS[D.coin.test.eosSys] = D.unit.eos.EOS
-
-const REQUEST_COINS = {}
-REQUEST_COINS[D.coin.main.btc] = 'BTC'
-REQUEST_COINS[D.coin.test.btcTestNet3] = 'BTC'
-REQUEST_COINS[D.coin.main.eth] = 'ETH'
-REQUEST_COINS[D.coin.test.ethRinkeby] = 'ETH'
-REQUEST_COINS[D.coin.main.eos] = 'EOS'
-REQUEST_COINS[D.coin.test.eosJungle] = 'EOS'
-REQUEST_COINS[D.coin.test.eosKylin] = 'EOS'
-REQUEST_COINS[D.coin.test.eosSys] = 'EOS'
+let UPDATE_DURATION = 60 * 60 * 1000
 
 export default class ExchangeCryptoCompareCom {
   constructor (exchange) {
@@ -30,27 +10,24 @@ export default class ExchangeCryptoCompareCom {
       throw D.error.invalidParams
     }
     this.provider = 'cryptocompare.com'
-    switch (exchange.coinType) {
-      case D.coin.main.btc:
-      case D.coin.test.btcTestNet3:
-      case D.coin.main.eth:
-      case D.coin.test.ethRinkeby:
-      case D.coin.main.eos:
-      case D.coin.test.eosJungle:
-      case D.coin.test.eosSys:
-      case D.coin.test.eosKylin:
-        this.coinType = exchange.coinType
-        break
-      default:
-        console.warn('ExchangeCryptoCompareCom don\'t support this coinType', exchange.coinType)
-        throw D.error.coinNotSupported
-    }
 
+    this.coinType = exchange.coinType
     this.exchange = D.copy(exchange)
-    this.exchange.unit = UNITS[exchange.coinType]
+    if (D.isBtc(exchange.coinType)) {
+      this.exchange.unit = D.unit.btc.BTC
+      this.requestCoin = 'BTC'
+    } else if (D.isEth(exchange.coinType)) {
+      this.exchange.unit = D.unit.eth.Ether
+      this.requestCoin = 'ETH'
+    } else if (D.isEos(exchange.coinType)) {
+      this.exchange.unit = D.unit.eos.EOS
+      this.requestCoin = 'EOS'
+    } else {
+      console.warn('ExchangeCryptoCompareCom don\'t support this coinType', exchange.coinType)
+      throw D.error.coinNotSupported
+    }
     this.exchange.exchange = exchange.exchange ||
       D.suppertedLegals().reduce((obj, currency) => (obj[currency] = 0) || obj, {})
-    this.requestCoin = REQUEST_COINS[exchange.coinType]
 
     // noinspection JSIgnoredPromiseFromCall
     this.updateExchange()
